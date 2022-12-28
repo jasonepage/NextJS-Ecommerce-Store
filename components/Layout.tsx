43485d8d65222c15bf0dc7ff64173f6d2a0262b7
jasonepage <jasonepage@gmail.com>
2022-12-27 19:46:43 -0800

    Listing Products.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,11 +5,11 @@ import React from "react";
 export default function Layout({
   children,
   title,
-}: { children: string } & { title?: string }) {
+}: { children: any } & { title?: string }) {
   return (
     <>
       <Head>
-        <title>{title ? title + "Amazona" : "Amazona"}</title>
+        <title>{title ? title + " - Amazona" : "Amazona"}</title>
         <meta name="description" content="Ecommerce Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -22,13 +22,19 @@ export default function Layout({
               <a className="text-lg font-bold">amazona</a>
             </Link>
             <div>
-              <Link href="/cart">Cart</Link>
-              <Link href="/login">Login</Link>
+              <Link legacyBehavior href="/cart">
+                <a className="p-2">Cart </a>
+              </Link>
+              <Link legacyBehavior href="/login">
+                <a className="p-2">Login </a>
+              </Link>
             </div>
           </nav>
         </header>
-        <main> {children} </main>
-        <footer>footer</footer>
+        <main className="container m-auto mt-4 px-4"> {children} </main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright 2023 Amazona</p>
+        </footer>
       </div>
     </>
   );
